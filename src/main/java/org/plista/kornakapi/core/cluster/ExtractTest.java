d66bdbcb34eeea80c47d5cc8893e340aa1ab09e4
maxweule <mw@plista.com>
2014-03-12 12:22:37 +0100

    loging

--- src/main/java/org/plista/kornakapi/core/cluster/ExtractTest.java
+++ src/main/java/org/plista/kornakapi/core/cluster/ExtractTest.java
@@ -16,6 +16,7 @@ public class ExtractTest {
 	public static void main(String [] args){
 		
 		String path = args[0];
+		System.out.print(path);
 		
 		File configFile = new File(path);
 		Configuration conf = null;
